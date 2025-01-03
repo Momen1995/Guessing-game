88fec2408f17b2fb7f0d8c5db6b663481a0186ec
Momen1995 <momensrk@gmail.com>
2025-01-03 11:03:22 +0600

    css style done and elemnt select done by js

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//element select
+const gameChange = document.querySelector("#game-change");
+const rangeCalc = document.querySelector(".range-calc");
+const attemptScore = document.querySelector(".attempt-score");
+const scoreBest = document.querySelector(".score-best");
+const inputNum = document.querySelector("#input-number");
+const guessBtn = document.querySelector(".guess");
+const newGameBtn = document.querySelector(".new-game");
+const avgScoreAttempt = document.querySelector(".avg-attempt");
+const gamePlays = document.querySelector(".game-plays");
+const gameRate = document.querySelector(".game-rate");
+const gameRange = document.querySelector(".game-range");
+
+gameChange.addEventListener("click",()=>{
+  console.log("click")
+})
